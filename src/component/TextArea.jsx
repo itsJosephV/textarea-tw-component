9fd7afabb8fa661c5021de8737fa98b4080ac8e7
Joseph <jvp.2703@gmail.com>
2023-12-12 17:59:44 +0000

    last commit

--- src/component/TextArea.jsx
+++ src/component/TextArea.jsx
@@ -17,9 +17,9 @@ const TextArea = () => {
   return (
 
       <form>
-        <div className="relative flex w-full flex-grow flex-col overflow-hidden rounded-md">
+        <div className="relative flex w-full overflow-hidden rounded-md">
           <textarea
-            className="m-0 max-h-[150px] h-[52px] w-full resize-none border-0 py-[10px] pl-3 pr-[50px] placeholder-white/50 bg-slate-700 text-white"
+            className="max-h-[150px] h-[52px] w-full resize-none py-[10px] pl-3 pr-[50px] placeholder-white/50 bg-slate-700 text-white"
             rows="1"
             tabIndex="0"
             placeholder="mdfka..."
