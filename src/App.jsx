4e87e6e01ffb979a3ad891a4c2831be4e6fad05a
Joseph <jvp.2703@gmail.com>
2023-12-12 17:17:31 +0000

    first and last

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TextArea from "./component/TextArea";
+
+function App() {
+  return (
+    <>
+      <TextArea />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/TextArea.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
+const TextArea = () => {
+
+  const textAreaRef = useRef(null);
+  const [letterValue, setLetterValue] = useState("");
+
+  const handleChange = (e) => {
+    setLetterValue(e.target.value);
+  };
+
+  useEffect(() => {
+    textAreaRef.current.style.height = "auto";
+    textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
+  }, [letterValue]);
+  
+  return (
+    <form
+      className="stretch mx-2 flex flex-row gap-3"
+      action=""
+    >
+      <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+        <div className="relative flex w-full flex-grow flex-col overflow-hidden rounded-md">
+          <textarea
+            className="m-0 max-h-[150px] h-[52px] w-full resize-none border-0 py-[10px] pl-3 pr-[50px] placeholder-white/50 bg-slate-700 text-white"
+            rows="1"
+            tabIndex="0"
+            placeholder="mdfka..."
+            value={letterValue}
+            onChange={handleChange}
+            ref={textAreaRef}
+          ></textarea>
+          <button className="absolute bottom-1.5 right-2 rounded-md bg-violet-500/80 hover:bg-violet-500 p-1 px-2 text-violet-200 hover:text-violet-100 transition-colors font-bold">
+            SE
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default TextArea
