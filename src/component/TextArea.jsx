c7ff7df03546aaa65d5c121b4dc323f588031486
Joseph <jvp.2703@gmail.com>
2023-12-12 21:11:12 +0000

    user-select

--- src/component/TextArea.jsx
+++ src/component/TextArea.jsx
@@ -9,13 +9,14 @@ const TextArea = () => {
     const textArea = textAreaRef.current;
     textArea.style.height = "auto";
     textArea.style.height = `${textArea.scrollHeight}px`;
+    // textArea.scrollTop = textArea.scrollHeight
   }, [letterValue]);
 
   return (
     <form className="mb-10">
       <div className="relative flex w-full overflow-hidden rounded-md">
         <textarea
-          className="max-h-[200px] h-[52px] w-full resize-none py-3.5 pr-12 pl-4 placeholder-white/50 bg-slate-700 text-white"
+          className="max-h-[200px] h-[52px] w-full resize-none py-3.5 pr-12 pl-4 placeholder-white/50 bg-slate-700 text-white !select-all"
           rows="1"
           tabIndex="0"
           placeholder="mdfka..."
